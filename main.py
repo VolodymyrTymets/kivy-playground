060bffe6e3d4ca4ed22730eb8a58a1ce77145235
Volodymyr Tymets <volodymyrtymets@gmail.com>
2023-12-18 15:40:26 +0200

    Mk format (#1)
    * first step
    
    * add .mv

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import kivy
 kivy.require('2.1.0') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+
+class LoginScreen(Widget):
+    pass
+     
+    def on_press(self):
+        print('---Press Buttom-->')
 
 
 class MyApp(App):
-
     def build(self):
-        return Label(text='Hello world')
-
+        return LoginScreen()
 
 if __name__ == '__main__':
     MyApp().run()
